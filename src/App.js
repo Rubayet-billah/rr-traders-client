17be5ad12db04222ccbf8efa2ca8e3bd66b43150
Rubayet-billah <rubayetbillah909@gmail.com>
2023-01-18 22:08:36 +0600

    ul added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi there! I am Rubayet.</h1>
+      <ul>
+        <li>It will be a showrooms website</li>
+        <li>It basically a construction items showroom</li>
+      </ul>
     </div>
   );
 }
